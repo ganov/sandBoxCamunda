1bb61d8db621b0c7505c47705eb3ecc1707dc947
Jean Ganovelli <ganovelli@gmail.com>
2021-04-19 22:22:25 -0400

    Start weather Delegate component: weatherAgent

---
+++ src/main/java/fr/ganov/camunda/WeatherDelegate.java
@@ -0,0 +1,21 @@
+package fr.ganov.camunda;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ */
+@Component("weatherAgent")
+public class WeatherDelegate implements JavaDelegate {
+
+    private final Logger logger = LogManager.getLogger(WeatherDelegate.class.getName());
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) {
+        logger.info("Hi! WeatherAgent speaking.");
+    }
+}
