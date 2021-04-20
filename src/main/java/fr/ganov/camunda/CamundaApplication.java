28ba9a8527f07f84790bb21280fbe02cf8128b1a
Jean Ganovelli <ganovelli@gmail.com>
2021-04-19 22:15:52 -0400

    Init Camunda project from Mvn archetype 'camunda-archetype-spring-boot-7.9.2'
    https://app.camunda.com/nexus/repository/camunda-bpm/archetype-catalog.xml

---
+++ src/main/java/fr/ganov/camunda/CamundaApplication.java
@@ -0,0 +1,13 @@
+package fr.ganov.camunda;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication("demoTuto")
+public class CamundaApplication {
+    public static void main(String... args) {
+        SpringApplication.run(CamundaApplication.class, args);
+    }
+}
---
+++ src/main/java/fr/ganov/camunda/LoggerDelegate.java
@@ -0,0 +1,27 @@
+package fr.ganov.camunda;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+/**
+ * This is an easy adapter implementation
+ * illustrating how a Java Delegate can be used
+ * from within a BPMN 2.0 Service Task.
+ */
+@Component("logger")
+public class LoggerDelegate implements JavaDelegate {
+
+    private final Logger logger = LogManager.getLogger(LoggerDelegate.class.getName());
+
+    public void execute(DelegateExecution execution) throws Exception {
+        logger.info("\n\n  ... LoggerDelegate invoked by " + "processDefinitionId=" + execution.getProcessDefinitionId()
+                + ", activtyId=" + execution.getCurrentActivityId() + ", activtyName='" + execution
+                .getCurrentActivityName() + "'" + ", processInstanceId=" + execution.getProcessInstanceId()
+                + ", businessKey=" + execution.getProcessBusinessKey() + ", executionId=" + execution.getId()
+                + " \n\n");
+    }
+
+}
